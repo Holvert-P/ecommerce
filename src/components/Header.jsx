b04a896d4a43c67089d79a24402db96cbb2591cb
Holvert Josué Pérez Talavera <50637750+Holvert-P@users.noreply.github.com>
2022-03-09 22:53:42 -0600

    m

---
+++ src/components/Header.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import "../styles/Header.scss";
+import logo from "../assets/logos/logo_yard_sale.svg";
+import icon_shopping_cart from "../assets/icons/icon_shopping_cart.svg";
+import icon_menu from "../assets/icons/icon_menu.svg";
+import Menu from "./Menu";
+
+const Header = () => {
+  const [toggle, setToggle] = useState(false);
+
+  const handleToggle = () => {
+    setToggle(!toggle);
+  };
+  return (
+    <nav>
+      <img src={icon_menu} alt="menu" className="menu" />
+      <div className="navbar-left nav-container">
+        <img src={logo} alt="logo" className="logo" />
+        <ul>
+          <li>
+            <a href="/">All</a>
+          </li>
+          <li>
+            <a href="/">Clothes</a>
+          </li>
+          <li>
+            <a href="/">Electronics</a>
+          </li>
+          <li>
+            <a href="/">Furnitures</a>
+          </li>
+          <li>
+            <a href="/">Toys</a>
+          </li>
+          <li>
+            <a href="/">Others</a>
+          </li>
+        </ul>
+      </div>
+      <div className="navbar-right">
+        <ul>
+          <li className="navbar-email" onClick={handleToggle}>
+            platzi@example.com
+          </li>
+          <li className="navbar-shopping-cart">
+            <img src={icon_shopping_cart} alt="shopping cart" />
+            <div>2</div>
+          </li>
+        </ul>
+      </div>
+      {toggle && <Menu />}
+    </nav>
+  );
+};
+
+export default Header;
